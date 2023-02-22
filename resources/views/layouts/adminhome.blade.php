0f039413bd90f19e303a8292bc50c3cdbe10e0fd
Heyner <89066219+leiva2021@users.noreply.github.com>
2023-02-22 11:18:02 -0600

    Se agrega el scroll a la vista income

--- resources/views/layouts/adminhome.blade.php
+++ resources/views/layouts/adminhome.blade.php
@@ -36,13 +36,13 @@
     <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
 
      <!-- Required Fremwork -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('./storage/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/storage/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
 
     <!-- Select 2 css -->
-    <link rel="stylesheet" href="{{ asset('./storage/bower_components/select2/dist/css/select2.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/storage/bower_components/select2/dist/css/select2.min.css') }}" />
     <!-- Multi Select css -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('./storage/bower_components/bootstrap-multiselect/dist/css/bootstrap-multiselect.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('./storage/bower_components/multiselect/css/multi-select.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('/storage/bower_components/bootstrap-multiselect/dist/css/bootstrap-multiselect.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('/storage/bower_components/multiselect/css/multi-select.css') }}" />
 
 
 
