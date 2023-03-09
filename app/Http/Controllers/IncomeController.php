832906238b36bf3ba834e1d7a7f40ca98920474a
Heyner <89066219+leiva2021@users.noreply.github.com>
2023-03-09 00:27:28 -0600

    Funcionalidad de ingresos

--- app/Http/Controllers/IncomeController.php
+++ app/Http/Controllers/IncomeController.php
@@ -23,14 +23,30 @@ class IncomeController extends Controller
         $temp_year = null;
         if (isset($request->year)) {
             $temp_year = $request->year;
-        }else{
+        } else {
             $temp_year = "vacio!";
         }
-            
 
-        return response()->json([
-            "temp_year" => $temp_year
-        ]);
+        //$anio = 2022;
+        // ? El model Book se cambia por Service
+        // ? date_book se cambia por date_service
+        /*$totalincomes = Book::select(DB::raw(
+            'SUM(IF(MONTH(date_book) = 1,  price, 0)) AS Ene,
+            SUM(IF(MONTH(date_book) = 2,  price, 0)) AS Feb,
+            SUM(IF(MONTH(date_book) = 3,  price, 0)) AS Mar,
+            SUM(IF(MONTH(date_book) = 4,  price, 0)) AS Abr,
+            SUM(IF(MONTH(date_book) = 5,  price, 0)) AS May,
+            SUM(IF(MONTH(date_book) = 6,  price, 0)) AS Jun,
+            SUM(IF(MONTH(date_book) = 7,  price, 0)) AS Jul,
+            SUM(IF(MONTH(date_book) = 8,  price, 0)) AS Ago,
+            SUM(IF(MONTH(date_book) = 9,  price, 0)) AS Sep,
+            SUM(IF(MONTH(date_book) = 10, price, 0)) AS Oct,
+            SUM(IF(MONTH(date_book) = 11, price, 0)) AS Nov,
+            SUM(IF(MONTH(date_book) = 12, price, 0)) AS Dic'
+        ))->whereYear('date_book', $temp_year)->get();*/
+
+        // * Se envia por JSON a $totalincomes
+        return response()->json($temp_year);
     }
     /**
      * Show the form for creating a new resource.
--- resources/js/incomereport.js
+++ resources/js/incomereport.js
@@ -18,11 +18,58 @@ const filter_income = (year) => {
     $.ajax({
         type: "POST",
         url: '/incomes/filter',
-        data: {year: year},
+        data: { year: year },
         headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
         dataType: "json",
         success: function (results) {
-            console.log(results);
+            $.each(results, function (index, result) {
+                let incomesByMonths = {
+                    "Enero": result.Ene == 0 || result.Ene == null ? '0' : result.Ene,
+                    "Febrero": result.Feb == 0 || result.Feb == null ? '0' : result.Feb,
+                    "Marzo": result.Mar == 0 || result.Mar == null ? '0' : result.Mar,
+                    "Abril": result.Abr == 0 || result.Abr == null ? '0' : result.Abr,
+                    "Mayo": result.May == 0 || result.May == null ? '0' : result.May,
+                    "Junio": result.Jun == 0 || result.Jun == null ? '0' : result.Jun,
+                    "Julio": result.Jul == 0 || result.Jul == null ? '0' : result.Jul,
+                    "Agosto": result.Ago == 0 || result.Ago == null ? '0' : result.Ago,
+                    "Setiembre": result.Sep == 0 || result.Sep == null ? '0' : result.Sep,
+                    "Octubre": result.Oct == 0 || result.Oct == null ? '0' : result.Oct,
+                    "Noviembre": result.Nov == 0 || result.Nov == null ? '0' : result.Nov,
+                    "Diciembre": result.Dic == 0 || result.Dic == null ? '0' : result.Dic
+
+                };
+                shows(incomesByMonths);
+                // TODO: console.log(`Mes = ${result.Dic}`) // devuele 0 o null
+            });
         }
     });
+}
+
+const shows = (incomesByMonths) => {
+    var cards = '';
+    for (var income in incomesByMonths) {
+
+        //console.log(`Mes #${income} ingreso = ${incomesByMonths[income]}`)
+        cards += `
+        
+        <div class="col-xl-3 col-md-6">
+                                <div class="card bg-c-green text-white">
+                                    <div class="card-block">
+                                        <div class="row align-items-center">
+                                            <div class="col">
+                                                <p class="m-b-5">${income}</p>
+                                                <h4 class="m-b-0">$${incomesByMonths[income]}</h4>
+                                            </div>
+                                            <div class="col col-auto text-end">
+                                                <i class="feather icon-credit-card f-50 text-c-green"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+        
+        `
+
+    }
+    $('#filter-incomes').html(cards);
 }
