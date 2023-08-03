059e78062b31a1f93e357bba073ce610f6852dda
Rayvolt8 <60490404+Rayvolt8@users.noreply.github.com>
2023-08-03 12:19:58 -0600

    add styles in suscription.blade part1
    se añadieron estilos a la vista de suscripción para ser mas congruentes con los demás estilos de la pagina
    https://www.notion.so/86340d8e031047068bcecfc2bd947437?v=c6b5ad9cf1bd4baab6d898b968ddcfee&p=bb71acb250704b60b5037ad049677547&pm=s

--- resources/views/dashboards/suscription.blade.php
+++ resources/views/dashboards/suscription.blade.php
@@ -1,176 +1,60 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+  @vite(['resources/css/suscriptionstyle.css'])
+
   <title>Pasarela de Suscripción</title>
-  <style>
-    /* Estilos para el formulario */
-    body {
-      background: url("https://images.unsplash.com/photo-1519500528352-2d1460418d41?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80");
-      background-color: rgba(0, 0, 0, 0.603);
-      font-family: Arial, sans-serif;
-      color: #747474; /* Texto en color blanco */
-    }
-    .form-group {
-    text-align: center;
-    margin-bottom: 10px;
-    border-color: #3452cc;
-  }
-
-  .form-group label {
-    display: block;
-    margin-bottom: 5px;
-  }
-
-  .form-group input[type="text"],
-  .form-group input[type="email"] {
-    text-align: center;
-    width: 90%;
-    padding: 14px;
-  }
-    .container {
-      max-width: 400px;
-      margin: 0 auto;
-      padding: 20px;
-    }
-
-    .subscription-form {
-      background-color: #fff;
-      border-radius: 8px;
-      padding: 50px;
-      box-shadow: 0 0 10px rgba(0, 50, 255, 0.8);
-    }
-
-    .form-group label {
-      font-weight: bold;
-      margin-bottom: 5px;
-    }
-
-    .form-group input,
-    .form-group select {
-      width: 100%;
-      padding: 10px;
-      margin-bottom: 10px;
-      border: 1px solid #ced4da;
-      border-radius: 4px;
-      font-size: 14px;
-    }
-
-    .form-group select {
-      appearance: none;
-    }
-
-    .form-group .card-input {
-      display: flex;
-      flex-wrap: wrap;
-      align-items: center;
-    }
-
-    .form-group .card-input > div {
-      flex: 1;
-    }
-
-    .form-group .card-input .card-brand {
-      width: 30px;
-      height: 30px;
-      background-size: contain;
-      background-repeat: no-repeat;
-      background-position: center;
-    }
-
-    .form-group .card-errors {
-      color: #dc3545;
-      margin-top: 5px;
-      font-size: 14px;
-    }
-
-    .form-group #card-element {
-      border: 1px solid #ced4da;
-      border-radius: 4px;
-      padding: 10px;
-    }
-
-    .form-group button {
-      background-color: #4266ff;
-      color: #fff;
-      border: none;
-      border-radius: 4px;
-      padding: 10px 20px;
-      font-size: 16px;
-      cursor: pointer;
-      transition: background-color 0.3s ease;
-    }
-
-    .form-group button:hover {
-      background-color: #3452cc;
-    }
-
-    /* Estilos para los mensajes de alerta */
-    .alert {
-      margin-bottom: 20px;
-      padding: 10px;
-      border-radius: 4px;
-      font-size: 14px;
-    }
-
-.alert-success {
-      background-color: #d4edda;
-      color: #155724;
-      border-color: #c3e6cb;
-    }
-
-    .alert-danger {
-      background-color: #f8d7da;
-      color: #721c24;
-      border-color: #f5c6cb;
-    }
-  </style>
 </head>
-<body>
-  <div class="container">
-    <div id="subscription-status" class="alert" style="display: none;"></div>
-    
-    <div class="subscription-form">
-      <form id="subscription-form" method="POST" action="{{ route('subscriptions.create') }}" onsubmit="return createSubscription(event)">
-        @csrf
-        
-        <h1 style="text-align: center">Pasarela de Suscripción</h1>
-        <div class="form-group">
-          <label for="name">Nombre:</label>
-          <input type="text" id="name" name="name" placeholder="Ingrese su nombre" required>
-        </div>
+<body style="background:url('https://images.pexels.com/photos/6353659/pexels-photo-6353659.jpeg'); background-size: cover; background-repeat: no-repeat!important; height: 100%; display: flex; justify-content: center; align-items: center; margin: 0; padding: 0; height: 100%; overflow: hidden;">
+  <div> 
+    <div class="container">
+      <div id="subscription-status" class="alert" style="display: none;"></div>
+      <div>
+        <form class="form-car" id="subscription-form" method="POST" action="{{ route('subscriptions.create') }}" onsubmit="return createSubscription(event)">
+          @csrf
+          
+          <h1 style="text-align: center">Pasarela de Suscripción</h1>
+          
+          <div class="form-group">
+            <div class="input-space">
+              <label for="name" id="name-label">Nombre:</label>
+              <input type="text" id="name" name="name" required>
+            </div>
+          </div>
 
-        <div class="form-group">
-          <label for="email">Correo electrónico:</label>
-          <input type="email" id="email" name="email" placeholder="Ingrese su correo electrónico" required>
-        </div>
+          <div class="form-group">
+            {{-- <label for="email">Correo electrónico:</label> --}}
+            <input type="email" id="email" name="email" placeholder="Ingrese su correo electrónico" required>
+          </div>
 
-        <div class="form-group">
-          <label for="barbers">Cantidad de barberos:</label>
-          <select id="barbers" name="barbers" required>
-            <option value="">Seleccionar el paquete de barberos</option>
-            <option value="1">1 barbero ($5000/mes)</option>
-            <option value="2">2-3 barberos ($7000/mes)</option>
-            <option value="3">4-6 barberos ($10000/mes)</option>
-          </select>
-        </div>
+          <div class="form-group">
+            {{-- <label for="barbers">Cantidad de barberos:</label> --}}
+            <select id="barbers" name="barbers" required>
+              <option value="">Seleccionar el paquete de barberos</option>
+              <option value="1">1 barbero ($5000/mes)</option>
+              <option value="2">2-3 barberos ($7000/mes)</option>
+              <option value="3">4-6 barberos ($10000/mes)</option>
+            </select>
+          </div>
 
-        <div class="form-group">
-          <label for="card-element">
-            Tarjeta de crédito:
-          </label>
-          <div id="card-element" class="card-input"></div>
-          <div id="card-errors" class="card-errors" role="alert"></div>
-        </div>
+          <div class="form-group">
+            <label for="card-element">
+              {{-- Tarjeta de crédito: --}}
+            </label>
+            <div id="card-element" class="card-input"></div>
+            <div id="card-errors" class="card-errors" role="alert"></div>
+          </div>
 
-        <div style="display: flex; justify-content: center;">
-          <button type="submit" style="background-color: rgba(0, 50, 255, 0.8); color: white; padding: 10px 20px; font-size: 16px; border: none; border-radius: 4px; cursor: pointer; transition: background-color 0.3s ease;">
-            Suscribirse
-          </button>
-        </div>
-      </form>
+          <div style="display: flex; justify-content: center;">
+            <button type="submit" style="background-color: salmon; color: white; padding: 10px 20px; font-size: 16px; border: none; border-radius: 4px; cursor: pointer; transition: background-color 0.3s ease;">
+              Suscribirse
+            </button>
+          </div>
+        </form>
+      </div>  
     </div>
   </div>
-
   <script src="https://js.stripe.com/v3/"></script>
   <script>
     var stripe = Stripe('pk_test_51NLptDFqcjWSUH4kd9ifw24ui4ydBYJ455Dp9nthFjCArI5qPzZbUE9inQnOYzwdhkAoXjH1hL1bhAQOMAbZmtmR00nVT9KxN3'); // Reemplaza 'pk_test_...' con tu clave pública de Stripe
@@ -232,6 +116,26 @@
       // Esto asegura que la alerta sea visible en la página
       alertElement.style.display = 'block';
     }
+// Agrega los escuchadores de eventos a los campos de entrada cuando el documento esté listo
+document.addEventListener('DOMContentLoaded', function() {
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach(function(input) {
+        input.addEventListener('input', function() {
+            checkInput(input);
+        });
+    });
+});
+
+// Tu función checkInput para aplicar los estilos a la etiqueta label cuando está en active
+function checkInput(input) {
+    const label = document.querySelector(`label[for="${input.id}"]`);
+    if (input.value.trim() !== '') {
+        label.classList.add('active');
+    } else {
+        label.classList.remove('active');
+    }
+}
+
   </script>
 </body>
 </html>
